09017f7d1e4531367f129243b6b290d676f63603
Silke Schürmann <jupiterfire@gmx.de>
2023-03-04 19:32:50 +0100

    Add key count and child adult sections

--- scripts/load_layouts.lua
+++ scripts/load_layouts.lua
@@ -1,8 +1,4 @@
-if HAS_KEYS then
-  Tracker:AddLayouts("layouts/dungeon_grids_keysanity.json")
-else
-  Tracker:AddLayouts("layouts/dungeon_grids.json")
-end
+Tracker:AddLayouts("layouts/dungeon_grids_keysanity.json")
 Tracker:AddLayouts("layouts/item_grids.json")
 if HAS_MAP then
   Tracker:AddLayouts("layouts/options.json")
