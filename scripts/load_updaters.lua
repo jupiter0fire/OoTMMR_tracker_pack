9f5da7e63f72826cf2ad6ead903da97c643e5d51
Wilhelm Schürmann <wimschuermann@googlemail.com>
2023-06-16 22:11:59 +0200

    Skip stages for certain MM items

--- scripts/load_updaters.lua
+++ scripts/load_updaters.lua
@@ -24,6 +24,25 @@ local OOTMM_SHARED_PREV = {
   ["Ocarina"] = {0},
   ["Wallets"] = {0},
 }
+local OOTMM_SKIP_PREV = {
+  ["MM_OCARINA"] = 0,
+  ["MM_SONG_GORON"] = 0,
+  ["MM_HOOKSHOT"] = 0,
+}
+local OOTMM_SKIP_STAGE = {
+  ["fairyOcarinaMm"] = {
+    ["item"] = "MM_OCARINA",
+    ["rule"] = function () return Tracker:ProviderCountForCode("setting_sharedOcarina_false") > 0 end
+  },
+  ["progressiveGoronLullaby"] = {
+    ["item"] = "MM_SONG_GORON",
+    ["rule"] = function () return true end
+  },
+  ["shortHookshotMm"] = {
+    ["items"] = "MM_HOOKSHOT",
+    ["rule"] = function () return true end
+  },
+}
 function tracker_on_accessibility_updating()
   if PACK_READY then
     -- Handle shared items in EmoTracker's GUI
@@ -39,6 +58,23 @@ function tracker_on_accessibility_updating()
           elseif mm_item.CurrentStage ~= OOTMM_SHARED_PREV[setting][i] then
             OOTMM_SHARED_PREV[setting][i] = mm_item.CurrentStage
             oot_item.CurrentStage = mm_item.CurrentStage
+          end
+        end
+      end
+    end
+
+    for setting, v in pairs(OOTMM_SKIP_STAGE) do
+      print(setting, v.item, v.rule())
+      if Tracker:ProviderCountForCode("setting_" .. setting .. "_true") > 0 then
+        if v.rule() then
+          local item = Tracker:FindObjectForCode(v.item)
+          print(item, "stage", item.CurrentStage, "prev", OOTMM_SKIP_PREV[v.item])
+          if item.CurrentStage > OOTMM_SKIP_PREV[v.item] then
+            item.CurrentStage = item.CurrentStage + 1
+            OOTMM_SKIP_PREV[v.item] = item.CurrentStage
+          elseif item.CurrentStage < OOTMM_SKIP_PREV[v.item] then
+            item.CurrentStage = item.CurrentStage - 1
+            OOTMM_SKIP_PREV[v.item] = item.CurrentStage
           end
         end
       end
