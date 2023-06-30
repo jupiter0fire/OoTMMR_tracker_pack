4023b5cf067a6968fe75fb0c28e38153f79124d0
Silke Schürmann <jupiterfire@gmx.de>
2023-06-30 19:45:42 +0200

    Split dungeon locations into oot and mm

--- scripts/load_locations.lua
+++ scripts/load_locations.lua
@@ -2,4 +2,5 @@
 Tracker:AddMaps("maps/maps.json")
 
 Tracker:AddLocations("locations/overworld.json")
-Tracker:AddLocations("locations/dungeons.json")
+Tracker:AddLocations("locations/mm_dungeons.json")
+Tracker:AddLocations("locations/oot_dungeons.json")
