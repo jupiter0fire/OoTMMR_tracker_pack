4c1dc005ceb6a2285409c82bcca6e88bbd4a8c3b
Kane Lives! <tsarchasm1@hotmail.com>
2024-03-10 10:34:27 +1030

    Update oot_logic.lua

--- scripts/oot_logic.lua
+++ scripts/oot_logic.lua
@@ -836,55 +836,55 @@ function _oot_logic()
 	end
 
 	function can_play_sun()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_SUN') and ('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_SUN') and ('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function can_play_time()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_TIME') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_A') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_TIME') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_A') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function can_play_epona()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_EPONA') and has_('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_EPONA') and has_('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function can_play_storms()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_STORMS') and has_('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_A')
+		return has_ocarina() and ('SONG_STORMS') and has_('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_A')
 	end
 
 	function can_play_zelda()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_ZELDA') and ('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_ZELDA') and ('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function can_play_saria()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_SARIA') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_SARIA') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function can_play_elegy()
-		return has_ocarina()  and ('SONG_EMPTINESS') and ('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_EMPTINESS') and ('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function can_play_tp_light() 
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_TP_LIGHT') and has_('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_TP_LIGHT') and has_('OOT_BUTTON_C-UP') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function can_play_tp_forest()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_TP_FOREST') and has_('OOT_BUTTON_C-UP') and ('OOT_BUTTON_A') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_TP_FOREST') and has_('OOT_BUTTON_C-UP') and ('OOT_BUTTON_A') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function can_play_tp_fire()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_TP_FIRE') and ('OOT_BUTTON_A') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_TP_FIRE') and ('OOT_BUTTON_A') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function can_play_tp_water()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_TP_WATER') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT') and ('OOT_BUTTON_A')
+		return has_ocarina() and ('SONG_TP_WATER') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT') and ('OOT_BUTTON_A')
 	end
 
 	function can_play_tp_shadow()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_TP_SHADOW') and has_('OOT_BUTTON_A') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_TP_SHADOW') and has_('OOT_BUTTON_A') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_C-LEFT') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function can_play_tp_spirit()
-		return has_ocarina() or has('SHARED_OCARINA') and ('SONG_TP_SPIRIT') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_A') and ('OOT_BUTTON_C-RIGHT')
+		return has_ocarina() and ('SONG_TP_SPIRIT') and ('OOT_BUTTON_C-DOWN') and ('OOT_BUTTON_A') and ('OOT_BUTTON_C-RIGHT')
 	end
 
 	function has_skeleton_key_oot()
