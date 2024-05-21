2fe8c1a54037dfe92e48c1f206f85c649d8ae10f
Silke Schürmann <jupiterfire@gmx.de>
2024-05-21 17:21:40 +0200

    Fix setting overwrite for starting age child

--- scripts/mm_logic.lua
+++ scripts/mm_logic.lua
@@ -440,7 +440,6 @@ function _mm_logic()
         ["erIndoors_none"] = true,
         ["erRegions_none"] = true,
         ["erIkanaCastle"] = false,
-        ["startingAge_child"] = true,
         ["smallKeyShuffleChestGame_vanilla"] = true,
         ["ageChange_none"] = true,
         ["progressiveSwordsOot_separate"] = true,
--- scripts/oot_logic.lua
+++ scripts/oot_logic.lua
@@ -440,7 +440,6 @@ function _oot_logic()
         ["erIndoors_none"] = true,
         ["erRegions_none"] = true,
         ["erIkanaCastle"] = false,
-        ["startingAge_child"] = true,
         ["smallKeyShuffleChestGame_vanilla"] = true,
         ["ageChange_none"] = true,
         ["progressiveSwordsOot_separate"] = true,
