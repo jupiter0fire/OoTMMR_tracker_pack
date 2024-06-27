8eaa9f176d7299f09721b223ff22cb4172f75738
Silke Schürmann <jupiterfire@gmx.de>
2024-06-27 21:00:08 +0200

    Add more MM grass checks

--- scripts/load_locations.lua
+++ scripts/load_locations.lua
@@ -1,6 +1,7 @@
 Tracker:AddMaps("maps/maps.json")
 
-Tracker:AddLocations("locations/overworld.json")
+Tracker:AddLocations("locations/oot_overworld.json")
+Tracker:AddLocations("locations/mm_overworld.json")
 Tracker:AddLocations("locations/mm_dungeons.json")
 Tracker:AddLocations("locations/oot_dungeons.json")
 if HAS_ER then
